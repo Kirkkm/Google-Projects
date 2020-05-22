1663332a6d9ce274b49a5a0e2b037448fb3971d6
Kirk <matthewkk7@gmail.com>
2020-05-22 07:54:25 -0400

    1st version of the image fix code for the 1st final project on Coursera (Google IT Automation with Python). This includes the code and some example images to run it against

---
+++ Google-imagefix.py
@@ -0,0 +1,32 @@
+import os
+from PIL import Image
+
+#setting variables to be used in the code
+size = 128, 128
+imgDir = 'Test Photos\\'
+fixedImgDir = 'Updated Photos\\'
+
+#this will pull a list of all the images we need to correct
+image_list = os.listdir(imgDir)
+
+#for troubleshooting, both print statement help with what direcotry the code is currently on
+print(os.getcwd())
+#for troubleshooting, both print statement help with what is save on the list
+print(image_list)
+
+
+
+
+#loops through all of the images in a give directory, as it loops through it changed the size and roates them
+#after that is done it saves the updated images in a seperate file
+for brokenImage in image_list:
+    #print("updated\\" + brokenImage)
+
+    bim = Image.open(imgDir + brokenImage)
+    fixedImage = bim.convert('RGB')
+    fixedImage.thumbnail(size)
+    #this was odd, in order for the rotation to save I had to call a save on the same line as the rotation
+    fixedImage.rotate(90,expand = 1).save(fixedImgDir + brokenImage,'JPEG')
+
+
+
