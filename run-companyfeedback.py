bc96f6ee318768026133851ed47f98ccce34a81e
Kirk <matthewkk7@gmail.com>
2020-05-26 13:16:20 -0400

    made corrections to the initializing of the dictionary and add the variable i to set & count up the ids of the feedback being posted

--- run-companyfeedback.py
+++ run-companyfeedback.py
@@ -2,28 +2,36 @@
 import os
 import requests
 
-feedback = {}
+feedback = []
 #pulls the list of files to read
-feedbackFiles = os.listdir('/data/feedback')
+feedbackFiles = os.listdir("/data/feedback")
 
+i = 2
 #loops through each file
 for files in feedbackFiles:
     #sets the filename as the key for the parent dictionary and the value is another dictionary
-    i = 2
-    feedback = {i,{}}
     #Reads each line in file and sets each line to the appropriate key for the child dictionary
-    with open(files, 'r') as f:
-        feedback[files] = {'title':f.readline()}
-        feedback[files] = {'name':f.readline()}
-        feedback[files] = {'date':f.readline()}
-        feedback[files] = {'feedback':f.read(4)}
+    with open("/data/feedback/" + files, "r") as f:
+        feedback.append({"title":f.readline().strip("\n"),
+                       "name":f.readline().strip("\n"),
+                       "date":f.readline().strip("\n"),
+                       "feedback":f.read().strip("\n")})
+        f.close()
     i += 1
 
 #Now to post the data in the dictionaries
-url = 'http://<corpweb-external-IP>/feedback'
+url = "http://<corpweb-external-IP>/feedback"
 
-data = requests.post(url, data=feedback)
+for userfeedback in feedback:
+    data = requests.post(url, data=userfeedback)
+    print(userfeedback)
+    print("")
+    #print(data.text)
+
+
 
 #Troubleshooting print statements
-print(data)
-print('Status code is ' + requests.status_codes())
+#print(feedback)
+#print(feedback.values())
+#print(data.text)
+print("Status code is " + str(requests.status_codes))
