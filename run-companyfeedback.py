d50eeee3983614fb117dcd2bb1562f6bf1e6babf
Kirk <matthewkk7@gmail.com>
2020-05-26 10:35:16 -0400

    made corrections to the initializing of the dictionary and add the variable i to set & count up the ids of the feedback being posted

--- run-companyfeedback.py
+++ run-companyfeedback.py
@@ -8,15 +8,16 @@ feedbackFiles = os.listdir('/data/feedback')
 
 #loops through each file
 for files in feedbackFiles:
-
     #sets the filename as the key for the parent dictionary and the value is another dictionary
-    feedback{files,{}}
+    i = 2
+    feedback = {i,{}}
     #Reads each line in file and sets each line to the appropriate key for the child dictionary
     with open(files, 'r') as f:
         feedback[files] = {'title':f.readline()}
         feedback[files] = {'name':f.readline()}
         feedback[files] = {'date':f.readline()}
         feedback[files] = {'feedback':f.read(4)}
+    i += 1
 
 #Now to post the data in the dictionaries
 url = 'http://<corpweb-external-IP>/feedback'
