eeeae8a1fc24da6cda1b550331261711541d277b
Kirk <matthewkk7@gmail.com>
2020-06-01 08:02:59 -0400

    Committing the code that is working now for the google project. Code can read files, put the data in dictionaries, and successfully POST them to the desired site.

--- run-companyfeedback.py
+++ run-companyfeedback.py
@@ -1,47 +1,39 @@
 #!/usr/bin/env python3
 import os
 import requests
-import json
 
-#feedback = []
 #pulls the list of files to read
-feedbackFiles = os.listdir("/data/feedback")
+feedbackFiles = os.listdir("D:\\Coding\\Google Certificate Project\\git-repo\\data\\feedback")
 
 #loops through each file
 for files in feedbackFiles:
     #sets the filename as the key for the parent dictionary and the value is another dictionary
     #Reads each line in file and sets each line to the appropriate key for the child dictionary
-    with open("/data/feedback/" + files, "r") as f:
-        '''
-        feedback.append({"title":f.readline().strip("\n"),
-                       "name":f.readline().strip("\n"),
-                       "date":f.readline().strip("\n"),
-                       "feedback":f.read().strip("\n")})
-        '''
+    with open("D:\\Coding\\Google Certificate Project\\git-repo\\data\\feedback\\" + files, "r") as f:
         feedbackPost = {"title":f.readline().strip("\n"),
                        "name":f.readline().strip("\n"),
                        "date":f.readline().strip("\n"),
                        "feedback":f.read().strip("\n")}
 
-        feedbackJSON = json.dumps(feedbackPost)
-
         f.close()
 
-        # Now to post the data in the dictionaries
-        #url = "http://<corpweb-external-IP>/feedback"
-        url = "http://35.222.41.65/feedback"
+        #Now to post the data in the dictionaries
+        #url = "http://httpbin.org/post" - public test url I found on StackOverflow
+        url = "http://<corpweb-external-IP>/feedback"
 
-        data = requests.post(url, json=feedbackJSON)
+        #posting the data
+        data = requests.post(url, data=feedbackPost)
 
+        #statement to raise any errors in the POST
         data.raise_for_status()
 
+        # Troubleshooting print statements
+        '''
         print("connection is " + str(requests.get(url)))
         print("")
         print("JSON data being sent is: ")
         print(feedbackJSON)
         print("Status code is " + str(data.status_code))
-
-
-#Troubleshooting print statements
-
-print("Status code is " + str(requests.status_codes))
+        print("ok status code is " + str(requests.codes.ok))
+        print(data.content)
+        '''
