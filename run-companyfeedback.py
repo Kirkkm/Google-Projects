57b112f0b0b231a82a2a8ad2d66603097cde3b3d
Kirk <matthewkk7@gmail.com>
2020-06-01 08:05:25 -0400

    Corrected comments in the code.

--- run-companyfeedback.py
+++ run-companyfeedback.py
@@ -2,29 +2,29 @@
 import os
 import requests
 
-#pulls the list of files to read
+# pulls the list of files to read
 feedbackFiles = os.listdir("D:\\Coding\\Google Certificate Project\\git-repo\\data\\feedback")
 
-#loops through each file
+# loops through each file
 for files in feedbackFiles:
-    #sets the filename as the key for the parent dictionary and the value is another dictionary
-    #Reads each line in file and sets each line to the appropriate key for the child dictionary
+    # sets the filename as the key for the parent dictionary and the value is another dictionary
+    # Reads each line in file and sets each line to the appropriate key for the child dictionary
     with open("D:\\Coding\\Google Certificate Project\\git-repo\\data\\feedback\\" + files, "r") as f:
-        feedbackPost = {"title":f.readline().strip("\n"),
-                       "name":f.readline().strip("\n"),
-                       "date":f.readline().strip("\n"),
-                       "feedback":f.read().strip("\n")}
+        feedbackPost = {"title": f.readline().strip("\n"),
+                        "name": f.readline().strip("\n"),
+                        "date": f.readline().strip("\n"),
+                        "feedback": f.read().strip("\n")}
 
         f.close()
 
-        #Now to post the data in the dictionaries
-        #url = "http://httpbin.org/post" - public test url I found on StackOverflow
+        # Now to post the data in the dictionaries
+        # url = "http://httpbin.org/post" - public test url I found on StackOverflow
         url = "http://<corpweb-external-IP>/feedback"
 
-        #posting the data
+        # posting the data
         data = requests.post(url, data=feedbackPost)
 
-        #statement to raise any errors in the POST
+        # statement to raise any errors in the POST
         data.raise_for_status()
 
         # Troubleshooting print statements
