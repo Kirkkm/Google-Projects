557f571e52a84d8a975aeb03bbf9513bc915611c
Kirk <matthewkk7@gmail.com>
2020-06-11 07:35:53 -0400

    Committing code I have written so far to be worked on later.

--- Final Project/changeImage.py
+++ Final Project/changeImage.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import PIL
+import os
 
 '''
 In this section, you will write a Python script named changeImage.py to process the supplier images. 
@@ -8,4 +9,29 @@ You will be using the PIL library to update all images within ~/supplier-data/im
 Size: Change image resolution from 3000x2000 to 600x400 pixel
 Format: Change image format from .TIFF to .JPEG
 
+After processing the images, save them in the same path ~/supplier-data/images, with a JPEG extension.
+
 '''
+
+size = 600, 400
+
+# sets the variables for the source images and fixed images path
+ImgDir ="/supplier-data/descriptions/images"
+FixedImgDir = "/supplier-data/descriptions/images"
+
+# pulls the list of images to be modified
+ImgList = os.listdir(ImgDir)
+
+######### Pulled from Google-imagefix.py code I wrote ##############
+# loops through all of the images in a give directory, as it loops through it changed the size and roates them
+# after that is done it saves the updated images in a seperate file
+for brokenImage in image_list:
+    #print("updated\\" + brokenImage)
+
+    bim = Image.open(imgDir + brokenImage)
+    fixedImage = bim.convert('RGB')
+    fixedImage.thumbnail(size)
+    # this was odd, in order for the rotation to save I had to call a save on the same line as the rotation
+    # also I could not find a way to rotate the image clockwise, only counter clockwise
+    fixedImage.rotate(270,expand = 1,).save(fixedImgDir + brokenImage,'JPEG')
+
