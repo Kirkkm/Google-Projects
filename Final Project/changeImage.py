7e59b7e90810cd4a0113741e44cd2e66d6389fa8
Kirk <matthewkk7@gmail.com>
2020-06-11 07:25:41 -0400

    Adding the shebange to each file and for changeImage.py added it's code's objective.

---
+++ Final Project/changeImage.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import PIL
+
+'''
+In this section, you will write a Python script named changeImage.py to process the supplier images. 
+You will be using the PIL library to update all images within ~/supplier-data/images directory to the following specifications:
+
+Size: Change image resolution from 3000x2000 to 600x400 pixel
+Format: Change image format from .TIFF to .JPEG
+
+'''
