0155ef592496f3dff47d9a3efb116609a8a788ab
Kirk <matthewkk7@gmail.com>
2020-06-19 07:37:41 -0400

    Committing the small progress I have made so far for setting up the system health check code so that if I need to I can pull the file somewhere else and continue working.

--- Final Project/health_check.py
+++ Final Project/health_check.py
@@ -1,1 +1,21 @@
 #!/usr/bin/env python3
+
+import psutil
+import shutil
+
+'''
+This is the last part of the lab, where you will have to write a Python script named health_check.py that will run
+in the background monitoring some of your system statistics: CPU usage, disk space, available memory and name resolution.
+Moreover, this Python script should send an email if there are problems, such as:
+
+    Report an error if CPU usage is over 80%
+    Report an error if available disk space is lower than 20%
+    Report an error if available memory is less than 500MB
+    Report an error if the hostname "localhost" cannot be resolved to "127.0.0.1"
+
+'''
+
+# variables for checking the
+cpu = psutil.cpu_percent()
+diskspace = psutil.disk_usage()
+free_memory = psutil.swap_memory().free
