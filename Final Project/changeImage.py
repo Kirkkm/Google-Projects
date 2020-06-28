f4a76adb6bfd6a4059af5c8f6c4d4b819f521dfc
Kirk <matthewkk7@gmail.com>
2020-06-28 10:51:57 -0400

    Committing second draft of the code that worked running on the VM.

--- Final Project/changeImage.py
+++ Final Project/changeImage.py
@@ -1,37 +1,33 @@
 #!/usr/bin/env python3
 from PIL import Image
 import os
-
-'''
-In this section, you will write a Python script named changeImage.py to process the supplier images. 
-You will be using the PIL library to update all images within ~/supplier-data/images directory to the following specifications:
-
-Size: Change image resolution from 3000x2000 to 600x400 pixel
-Format: Change image format from .TIFF to .JPEG
-
-After processing the images, save them in the same path ~/supplier-data/images, with a JPEG extension.
-
-'''
+import re
 
 size = 600, 400
 
 # sets the variables for the source images and fixed images path
-ImgDir = "/supplier-data/images/"
-FixedImgDir = "/supplier-data/images/"
+ImgDir = "supplier-data/images/"
+FixedImgDir = "supplier-data/images/"
 
 # pulls the list of images to be modified
 ImgList = os.listdir(ImgDir)
 
+# filters the list using regex to only pull all the tiff files into a list
+TIFF_finder = re.compile(".*\.tiff$")
+TIFF_List = list(filter(TIFF_finder.search, ImgList))
+
+print (TIFF_List)
 
 # loops through all of the images in a given directory, as it loops through it changes the size
 # after that is done it saves the updated images in a separate file
-for originalImage in ImgList:
+for originalImage in TIFF_List:
     # print("updated\\" + brokenImage)
+
+    print(ImgDir + originalImage)
 
     bim = Image.open(ImgDir + originalImage)
     fixedImage = bim.convert('RGB')
     fixedImage.thumbnail(size)
-    # also I could not find a way to rotate the image clockwise, only counter clockwise
-    fixedImage.save(FixedImgDir + originalImage,'JPEG')
+    fixedImage.save(FixedImgDir + originalImage.replace('tiff','jpeg'))
 
     print("Image reformatted")
